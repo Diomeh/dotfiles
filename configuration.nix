aefa3b774154d97baa291f308f1baedf5e2a1ff9
David Urbina <davidurbina.dev@gmail.com>
2024-06-10 12:12:12 -0500

    Disabled ipv6 temporarily because of connection failures

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,9 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # IPV6 fails to connect and is unreachable from https://ipleak.net/, disable it for lack of better solution for now.
+  networking.enableIPv6 = false;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
