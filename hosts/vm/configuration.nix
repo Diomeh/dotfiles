86759e5e79a5e894d1e23aadb6c1e71589e9f044
David Urbina <davidurbina.dev@gmail.com>
2024-06-15 21:11:29 +0000

    Enabled `nh` utility and `zsh` shell

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -18,6 +18,11 @@
 	# Enable experimental features
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  environment.sessionVariables = {
+    # For nix helper
+    FLAKE = "/home/demo/dotfiles";
+  };
+
   # Let demo build as a trusted user.
   # nix.settings.trusted-users = [ "demo" ];
 
@@ -50,7 +55,50 @@
     vim
     git
 		vscode-fhs
+    eza
+    nh # Nix Helper
+    kate
   ];
+
+  users.users.demo = {
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    zsh-autoenv.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "agnoster";
+      plugins = [
+        "git"
+        "npm"
+        "history"
+        "node"
+        "rust"
+        "deno"
+        "docker"
+        "web-search"
+        "direnv"
+      ];
+    };
+    shellAliases = {
+      ls = "${pkgs.eza}/bin/eza --git --icons"; # Display icons and git status
+      l = "ls -lahbX"; # Long format, all files, show header, binary size prefix, dereference symlinks
+      ll = "ls -l"; # Long format
+      la = "ls -a"; # All files
+      lt = "ls --tree"; # Tree format
+      ldir = "l -D"; # Directories only
+      lfil = "l -f"; # Files only
+    };
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
