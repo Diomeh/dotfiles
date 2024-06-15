ad303a5b7fc7ba2ac03ffe00356cb8456c3960a3
David Urbina <davidurbina.dev@gmail.com>
2024-06-15 15:06:42 -0500

    Removed redundant flake config

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
   outputs = { self, nixpkgs, ... }@inputs: {
     # Match configuration name to system hostname for automatic selection
     nixosConfigurations = {
-	nixos = nixpkgs.lib.nixosSystem {
-		modules = [
-			./hosts/vm/configuration.nix
-		];
-	};
       victus16d00 = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
         modules = [
