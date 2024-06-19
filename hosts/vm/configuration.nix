7db78837b2a8e4b2bdd11e428e905bd1ba07e31f
David Urbina <davidurbina.dev@gmail.com>
2024-06-19 15:50:52 -0500

    Updated `vm` host config file

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -5,159 +5,68 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../modules/nixos/hyprland.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+
+    # Import drivers
+    ../../modules/drivers/opengl.nix
+#     ../../modules/drivers/nvidia.nix
+#     ../../modules/drivers/power-management.nix
+
+    # Import users
+    ../../modules/users/diomeh.nix
+
+    # Import settings
+    ../../modules/settings/bluetooth.nix
+    ../../modules/settings/cups.nix
+#     ../../modules/settings/firewall.nix
+    ../../modules/settings/fonts.nix
+#     ../../modules/settings/fstrim.nix
+    ../../modules/settings/home-manager.nix
+    ../../modules/settings/locales.nix
+    ../../modules/settings/network.nix
+    ../../modules/settings/sound.nix
+    ../../modules/settings/x11.nix
+
+    # Import DE
+    # ../../modules/settings/desktops/kde5.nix
+    ../../modules/settings/desktops/kde6.nix
+    # ../../modules/settings/desktops/hyprland.nix
+
+    # Import packages
+    ../../modules/packages/browsers.nix
+    ../../modules/packages/development.nix
+#     ../../modules/packages/direnv.nix
+#     ../../modules/packages/docker.nix
+#     ../../modules/packages/gaming.nix
+#     ../../modules/packages/libvirt.nix
+    ../../modules/packages/misc.nix
+#     ../../modules/packages/mullvad.nix
+    ../../modules/packages/nix-helper.nix
+    ../../modules/packages/nix-ld.nix
+#     ../../modules/packages/office.nix
+    ../../modules/packages/package-managers.nix
+#     ../../modules/packages/samba.nix
+#     ../../modules/packages/steam.nix
+    ../../modules/packages/sys-utils.nix
+#     ../../modules/packages/virtualbox.nix
+#     ../../modules/packages/wine.nix
+    ../../modules/packages/zsh.nix
+
+    # Import specialisations
+#     ../../modules/specialisations/on-the-go.nix
+  ];
 
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "vm"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Bogota";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_CO.UTF-8";
-    LC_IDENTIFICATION = "es_CO.UTF-8";
-    LC_MEASUREMENT = "es_CO.UTF-8";
-    LC_MONETARY = "es_CO.UTF-8";
-    LC_NAME = "es_CO.UTF-8";
-    LC_NUMERIC = "es_CO.UTF-8";
-    LC_PAPER = "es_CO.UTF-8";
-    LC_TELEPHONE = "es_CO.UTF-8";
-    LC_TIME = "es_CO.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable SDDM display manager
-  services.displayManager.sddm.enable = true;
-
-  # Enable KDE 5
-  services.xserver.desktopManager.plasma5.enable = false;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "altgr-intl";
-  };
-
-	# Enable experimental features
+  # Enable nix commands and flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  environment.sessionVariables = {
-    # For nix helper
-    FLAKE = "/home/demo/dotfiles";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.diomeh = {
-    isNormalUser = true;
-    description = "diomeh";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
-    packages = with pkgs; [];
-  };
-
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "diomeh";
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    git
-    vscode-fhs
-    eza
-    nh # Nix Helper
-    kate
-  ];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    zsh-autoenv.enable = true;
-    syntaxHighlighting.enable = true;
-    ohMyZsh = {
-      enable = true;
-      theme = "agnoster";
-      plugins = [
-        "git"
-        "npm"
-        "history"
-        "node"
-        "rust"
-        "deno"
-        "docker"
-        "web-search"
-        "direnv"
-      ];
-    };
-    shellAliases = {
-      ls = "${pkgs.eza}/bin/eza --git --icons"; # Display icons and git status
-      l = "ls -lahbX"; # Long format, all files, show header, binary size prefix, dereference symlinks
-      ll = "ls -l"; # Long format
-      la = "ls -a"; # All files
-      lt = "ls --tree"; # Tree format
-      ldir = "l -D"; # Directories only
-      lfil = "l -f"; # Files only
-    };
-  };
-
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      "diomeh" = import ./home.nix;
-    };
-  };
+  # Define your hostname.
+  networking.hostName = "vm";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
