0c9af96ce8be8aaf0bc0c36f3e390cd60ccfdfd5
David Urbina <davidurbina.dev@gmail.com>
2024-06-25 23:45:59 -0500

    Added `home-manager` package

--- modules/settings/home-manager.nix
+++ modules/settings/home-manager.nix
@@ -1,10 +1,14 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 let
   # TODO: read user from a separate file
   username = "diomeh";
   hostname = "victus16d00";
 in
 {
+  environment.systemPackages = with pkgs; [
+    home-manager
+  ];
+
   # home-manager configuration
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
