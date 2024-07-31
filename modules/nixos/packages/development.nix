824a8503fdd984a457c08bbff6da124a8b84cf0f
David Urbina <davidurbina.dev@gmail.com>
2024-07-31 12:54:03 -0500

    replaced jetbrains toolbox with specific IDEs

--- modules/nixos/packages/development.nix
+++ modules/nixos/packages/development.nix
@@ -2,7 +2,6 @@
 {
   environment.systemPackages = with pkgs; [
     vscode-fhs # Visual Studio Code with FHS support
-    jetbrains-toolbox # JetBrains Toolbox
     gitkraken # GitKraken Git GUI
     rustup # Rust programming language
     bacon # Background rust code checker
@@ -17,5 +16,9 @@
     shfmt # Shell script formatter
     shellcheck # Shell script linter
     blender # 3D creation suite
+    jetbrains.webstorm # Web development IDE
+    jetbrains.rust-rover # Rust IDE
+    jetbrains.pycharm-professional # Python IDE
+    jetbrains.phpstorm # PHP IDE
   ];
 }
