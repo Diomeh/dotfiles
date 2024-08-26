0bb8aaf29c67498f8740045c5933ed7d594ee1fb
David Urbina <davidurbina.dev@gmail.com>
2024-08-26 17:21:25 -0500

    removed unnecesary packages

--- modules/nixos/packages/development.nix
+++ modules/nixos/packages/development.nix
@@ -3,20 +3,12 @@
   environment.systemPackages = with pkgs; [
     vscode-fhs # Visual Studio Code with FHS support
     gitkraken # GitKraken Git GUI
-    rustup # Rust programming language
-    bacon # Background rust code checker
     glab # GitLab CLI
     bruno # API testing tool
     beekeeper-studio # SQL GUI
     dbeaver-bin # SQL GUI
     kate # Kate text editor
     sublime4 # Sublime Text
-    zed-editor # Zed text editor
-    obsidian # Obsidian note-taking app
-    shfmt # Shell script formatter
-    shellcheck # Shell script linter
-    shunit2 # Shell script unit testing framework
-    blender # 3D creation suite
     gnome.zenity # Tool to display dialogs from the commandline and shell scripts
     jetbrains.webstorm # Web development IDE
     jetbrains.rust-rover # Rust IDE
