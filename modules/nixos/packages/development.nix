5e531f7000377b4705c3f3f9e8f600f66f1c56b5
David Urbina <davidurbina.dev@gmail.com>
2024-09-28 10:30:37 -0500

    Moved jetbrains IDE's to nixpkgs unstable

--- modules/nixos/packages/development.nix
+++ modules/nixos/packages/development.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  unstable = import <nixos-unstable> {
+    # reuse the current configuration
+    config = config.nixpkgs.config;
+  };
+in
 {
   environment.systemPackages = with pkgs; [
     vscode-fhs # Visual Studio Code with FHS support
@@ -10,10 +16,10 @@
     kate # Kate text editor
     sublime4 # Sublime Text
     gnome.zenity # Tool to display dialogs from the commandline and shell scripts
-    jetbrains.webstorm # Web development IDE
-    jetbrains.rust-rover # Rust IDE
-    jetbrains.pycharm-professional # Python IDE
-    jetbrains.phpstorm # PHP IDE
+    unstable.jetbrains.webstorm # Web development IDE
+    unstable.jetbrains.rust-rover # Rust IDE
+    unstable.jetbrains.pycharm-professional # Python IDE
+    unstable.jetbrains.phpstorm # PHP IDE
     starship # Cross-shell prompt
   ];
 }
