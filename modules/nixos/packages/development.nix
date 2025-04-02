43cfb83c8aeb0f7ce75135d292067b2c7a08b223
David Urbina <davidurbina.dev@gmail.com>
2025-04-02 17:25:38 -0500

    add android studio

--- modules/nixos/packages/development.nix
+++ modules/nixos/packages/development.nix
@@ -20,6 +20,7 @@ in
     unstable.jetbrains.rust-rover # Rust IDE
     unstable.jetbrains.pycharm-professional # Python IDE
     unstable.jetbrains.phpstorm # PHP IDE
+    unstable.android-studio # Official IDE for Android
     starship # Cross-shell prompt
     nurl # Nixos URL fetcher generator
     nix-init # Nixos package generator
